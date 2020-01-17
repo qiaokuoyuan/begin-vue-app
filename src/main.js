398ed3140c75d58ddc110eb33222ef13d6ef1417
QQY <457361577@qq.com>
2020-01-17 17:05:07 +0800

    Update main.js

--- src/main.js
+++ src/main.js
@@ -1,8 +1,25 @@
 import Vue from 'vue'
 import App from './App.vue'
+import Router from 'vue-router'
+
+Vue.use(Router)
 
 Vue.config.productionTip = false
 
 new Vue({
   render: h => h(App),
 }).$mount('#app')
+
+
+
+
+export const constantRouterMap = [
+  {
+    path: "/test",
+    component: () => import("@/components/test")
+  }
+]
+
+export default new Router({
+  routes: constantRouterMap
+})
